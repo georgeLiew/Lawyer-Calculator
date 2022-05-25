5a1665393a9b6081af08001d757b07e59c395813
Liew Jun Kang <ljk_jgs@hotmail.com>
2022-05-25 22:34:34 +0800

    add calc stamp duty function

--- src/page/LegalFeeCalc.vue
+++ src/page/LegalFeeCalc.vue
@@ -1,28 +1,13 @@
 <template>
 <div class="from_box">
-    <h1>Legal Fee Calculate</h1>
+    <h1>Legal Fee Calculator</h1>
     <div class="form-group">
-        <inputNumber labelName="Purchase Price" v-model:modelValue="buyerCalc.purchasePrice" @updateValue="onPurchasePriceChange"></inputNumber>
-        <inputNumber labelName="Actual Price" v-model="prop.price" @updateValue="onPropertyPriceChange"></inputNumber>
-        <inputNumber labelName="Loan Amount" v-model="buyerCalc.loanAmount"></inputNumber>
-        <inputNumber labelName="Paid Deposit" v-model="buyerCalc.depositPaid" @updateValue="onPaidDepositChange"></inputNumber>
-        <inputNumber labelName="Deposit Balance" v-model="buyerCalc.depositBalance"></inputNumber>
-        <inputNumber labelName="Lega Fee" v-model="buyerCalc.legal_fee"></inputNumber>
-        <inputNumber labelName="Agent Commission" v-model="buyerCalc.agent_commission"></inputNumber>
-        <button type="button" v-on:click="CalculateBuyerBalance">Calculate buyer balance</button>
+        <inputNumber labelName="Property Purchase Price" v-model:modelValue="legalFee.purchasePrice" @updateValue="onPurchasePriceChange"></inputNumber>
     </div>
     <div class="result form-group">
-        <customLabelNumber labelName="Purchase Price" v-model="buyerCalc.purchasePrice"></customLabelNumber>
-        <customLabelNumber labelName="Actual Price" v-model="prop.price"></customLabelNumber>
-        <customLabelNumber labelName="Loan Amount" v-model="buyerCalc.loanAmount"></customLabelNumber>
-        <customLabelNumber labelName="Total Deposit" v-model="prop.totalDeposit"></customLabelNumber>
-        <customLabelNumber labelName="Deposit Paid" v-model="buyerCalc.depositPaid"></customLabelNumber>
-        <customLabelNumber labelName="Deposit Balance" v-model="buyerCalc.depositBalance"></customLabelNumber>
-        <customLabelNumber labelName="Legal Fee" v-model="buyerCalc.legal_fee"></customLabelNumber>
-        <customLabelNumber labelName="Agent Commission" v-model="buyerCalc.agent_commission"></customLabelNumber>
-        <div v-if="buyerCalc.showBalance"> 
-        <customLabelNumber labelName="Buyer Balance" v-model="buyerCalc.balance"></customLabelNumber>
-        </div>
+        <customLabelNumber labelName="Property Purchase Price" v-model="legalFee.purchasePrice"></customLabelNumber>
+        <customLabelNumber labelName="Stamp duty" v-model="legalFee.stampduty"></customLabelNumber>
+        <customLabelNumber labelName="Total Legal Fees Payable" v-model="legalFee.legalFeeAmount"></customLabelNumber>
     </div>
 </div>
 </template>
@@ -35,24 +20,12 @@ export default {
     name: 'Form',
     data() {
         return {
-            prop: {
-                "price": 0,
-                "totalDeposit": 0,
-            },
-            buyerCalc: {
+            legalFee: {
                 "purchasePrice": 0,
-                "loanAmount": 0,
-                "depositPaid": 0,
-                "depositBalance": 0,
-                "legal_fee": 0,
-                "agent_commission": 0,
-                "showBalance" : false,
-                "balance": 0
-            },
-            sellerCalc: {
-                "legal_fee": 0,
-                "agent_comission": 0
+                "legalFeeAmount": 0,
+                "stampduty":0
             }
+
         };
     },
     methods: {
@@ -63,26 +36,41 @@ export default {
             }
         },
         onPurchasePriceChange(e) {
-            this.buyerCalc.loanAmount = e * 0.9;
-        },
-        onPropertyPriceChange(e) {
-            this.prop.totalDeposit = e * 0.1;
-            this.buyerCalc.depositPaid = e * 0.01;
-            this.buyerCalc.depositBalance = this.prop.totalDeposit - this.buyerCalc.depositPaid;
-        },
-        onPaidDepositChange(e) {
-            this.buyerCalc.depositBalance = this.prop.totalDeposit - e;
-        },
-        CalculateBuyerBalance() {
-            this.buyerCalc.showBalance =true;
-            this.buyerCalc.balance = parseFloat(this.buyerCalc.loanAmount) - parseFloat(this.prop.price) + parseFloat(this.buyerCalc.depositPaid) - parseFloat(this.buyerCalc.legal_fee) - parseFloat(this.buyerCalc.agent_commission);
-        }
+            this.legalFee.stampduty = calcStampDuty(e);
+        }        
     },
     components: {
         inputNumber,
         customLabelNumber
     }
 }
+
+
+function calcStampDuty(amount){
+            var stampduty = 0;
+            var firstLevelThreadhold = 100000;
+            var secondLevelThreadhold = 500000;
+            var thirdLevelThreadhold = 1000000;
+        
+            if(amount >0){
+                if(amount < firstLevelThreadhold){
+                    stampduty += amount * 0.01;
+                }else if(amount < secondLevelThreadhold){
+                    stampduty += firstLevelThreadhold * 0.01;
+                    stampduty += (amount - firstLevelThreadhold) * 0.02;
+                }else if (amount < thirdLevelThreadhold){
+                    stampduty += firstLevelThreadhold * 0.01;
+                    stampduty += (secondLevelThreadhold - firstLevelThreadhold) * 0.02;
+                    stampduty += (amount - secondLevelThreadhold) * 0.03;
+                }else{
+                    stampduty += firstLevelThreadhold * 0.01;
+                    stampduty += (secondLevelThreadhold - firstLevelThreadhold) * 0.02;
+                    stampduty += (thirdLevelThreadhold - secondLevelThreadhold) * 0.03;
+                    stampduty += (amount - thirdLevelThreadhold) * 0.04;
+                }
+            } 
+            return stampduty;           
+        }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
